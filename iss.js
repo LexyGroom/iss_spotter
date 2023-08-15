a8c6e96bf6defcc83288c148d70d4a4877bf443c
Alexys Groom <lexygroom@gmail.com>
2023-08-14 22:05:49 -0400

    Implement iss

---
+++ iss.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+
+    let ip = JSON.parse(body);
+    ip = ip.ip;
+    return callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
